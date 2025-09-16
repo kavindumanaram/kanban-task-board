516178a99c8588e84938b5bb83ea543a08752eb9
Dimuth Kavindu Manaram <kavinduxyz@gmail.com>
2025-09-16 21:27:43 +1000

    allowed all cores for dev purpose

--- backend/Kanban.Api/Program.cs
+++ backend/Kanban.Api/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000") // React dev server
+            policy.WithOrigins()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
